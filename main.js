63d5aba98caa36e04708ec85f9e94504895d2dc0
Henrikkittang <42532845+Henrikkittang@users.noreply.github.com>
2020-03-27 11:57:05 +0100

    Update main.js
    added changes ;-)

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ app.listen(8030, () => {console.log(`listening at port 8030`)});
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
-console.log('Hello there');
+console.log('how you doin?');
