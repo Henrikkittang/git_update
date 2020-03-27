878ada051c4c632e8e902e82f5296ad618cc34a4
Henrikkittang <42532845+Henrikkittang@users.noreply.github.com>
2020-03-27 11:55:52 +0100

    yhehe

--- update.py
+++ update.py
@@ -1,6 +1,18 @@
+import schedule
+import time
+
+def test():
+    print('hello there')
 
 
+schedule.every(5).seconds.do(test)
 
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+"""
 with open('main.js', 'r') as file:
     data = file.read()
     file.close()
@@ -10,3 +22,4 @@ with open('main.js', 'r') as file:
 with open('main.js', 'w') as file:
     file.write(data)
     file.close()
+"""
