a2dc795b9b0b73367c7c8555ccd323eb70e46d22
Henrikkittang <42532845+Henrikkittang@users.noreply.github.com>
2020-03-29 19:32:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.listen(8050, () => {console.log(`listening at port 8050`)});
 app.use(express.static('public'));
 app.use(express.json({limit: '2mb'}));
 
-
+// hello there, didnt see you there
 const jobb = schedule.scheduleJob('* */15 * * * *', () => {        
     const { spawn } = require('child_process');
     const pyProg = spawn('python', ['update.py']);
